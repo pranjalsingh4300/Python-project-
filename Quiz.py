f8eab8461ca5a6e1c4666c7c0655d38b2cad49d2
pranjalsingh4300 <pranjalsingh4300@gmail.com>
2026-09-20 11:52:05 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.write('hello,I am arjan welcome to my quiz zone. hope you like the game. you may please proceed further for gaming...')
+age = str.num_input('enter you age')
+if age >=18
+  str.write(you are eligible for licence...')
+  str.balloos()
+  str.snow()
+else 
+str.write("you qre not eligible")
